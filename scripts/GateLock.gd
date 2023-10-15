bf7570078bafced26dec8eccd8d5238a0f08a6e5
Luccas Rocigno <luccasrocigno@hotmail.com>
2023-10-14 23:24:39 -0300

    Creating GateLock class

--- scripts/GateLock.gd
+++ scripts/GateLock.gd
@@ -2,5 +2,24 @@ class_name GateLock
 
 extends Node2D
 
-func move(dir):
-	pass
+var _lock_speed = 100.0
+var _max_movement = 32
+
+enum LockDirection {UP, DOWN, LEFT, RIGHT}
+
+func move(delta, direction: LockDirection):
+	var finished_movement: bool
+	var move_amount = _lock_speed * delta
+
+	match direction:
+		LockDirection.UP:
+			position.y -= move_amount
+
+		LockDirection.DOWN:
+			position.y += move_amount
+
+		LockDirection.LEFT:
+			position.x -= move_amount
+
+		LockDirection.RIGHT:
+			position.x += move_amount
