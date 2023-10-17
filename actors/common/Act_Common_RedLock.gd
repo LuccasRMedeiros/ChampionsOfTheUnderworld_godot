aafea300e2d54328eb24008c8c7e69dafdc50de7
Luccas Rocigno <luccasrocigno@hotmail.com>
2023-10-17 19:41:01 -0300

    Created gate mechanics

---
+++ actors/common/Act_Common_RedLock.gd
@@ -0,0 +1,3 @@
+class_name GateLock_Red
+
+extends GateLock
--- actors/common/Act_Common_SpawnGate.gd
+++ actors/common/Act_Common_SpawnGate.gd
@@ -1,85 +1,103 @@
 extends Node2D
 
-enum SelectorDirection {FORWARD, BACKWARD}
-enum Orientation {HORIZONTAL, VERTICAL}
 enum GateState {LOCKED, OPEN}
+enum CursorStates {SELECTING, MOVING}
+enum CursorDirections {FORWARD, BACKWARD}
+enum CursorOrientations {HORIZONTAL, VERTICAL}
 
-var _current_orientation = Orientation.HORIZONTAL
-var _warp_amount = 24
+var _next_warp = 24
 var _loop_warp = -48
 var _gate_state = GateState.LOCKED
-var _lock_array = [
+var _cursor_state = CursorStates.SELECTING
+var _lock_matrix = [
+	[Definitions, Definitions, Definitions],
+	[Definitions, Definitions, Definitions],
+	[Definitions, Definitions, Definitions]
+	]
+var _lock_node_matrix = [
+	[GateLock, GateLock, GateLock],
+	[GateLock, GateLock, GateLock],
+	[GateLock, GateLock, GateLock]
+	]
+
+var cursor_orientation = CursorOrientations.HORIZONTAL
+var rc_position = 1
+
+func move_cursor(direction: CursorDirections):
+	var cursor_warp: Vector2
+	var warp_amount = _next_warp
+
+	if direction == CursorDirections.FORWARD:
+		rc_position = rc_position + 1 
+
+		if rc_position == 3:
+			rc_position = 0
+			warp_amount = _loop_warp
+		
+		warp_amount *= -1
+
+	elif direction == CursorDirections.BACKWARD:
+		rc_position = rc_position - 1
+		
+		if rc_position == -1:
+			rc_position = 2
+			warp_amount = _loop_warp
+
+	if cursor_orientation == CursorOrientations.HORIZONTAL:
+		cursor_warp = Vector2(0.0, float(warp_amount))
+	elif cursor_orientation == CursorOrientations.VERTICAL:
+		cursor_warp = Vector2(float(warp_amount), 0.0)
+
+	$Cursor.position -= cursor_warp
+
+func change_cursor_orientation():
+	var position_bckp = $Cursor.position
+	
+	$Cursor.position.x = position_bckp.y
+	$Cursor.position.y = position_bckp.x
+
+	if cursor_orientation == CursorOrientations.HORIZONTAL:
+		$Cursor.rotation_degrees = 90.0
+		cursor_orientation = CursorOrientations.VERTICAL
+	else:
+		$Cursor.rotation_degrees = 0
+		cursor_orientation = CursorOrientations.HORIZONTAL
+
+func shuffle_matrix():
+	var lock_array = [
 	Definitions.LockColors.RED, Definitions.LockColors.RED, Definitions.LockColors.RED,
 	Definitions.LockColors.GREEN, Definitions.LockColors.GREEN, Definitions.LockColors.GREEN,
 	Definitions.LockColors.BLUE, Definitions.LockColors.BLUE, Definitions.LockColors.BLUE
 	]
-var _lock_matrix = [
-	[Node, Node, Node],
-	[Node, Node, Node],
-	[Node, Node, Node]
-	]
-
-func move_selector(direction: SelectorDirection):
-	if _current_orientation == Orientation.HORIZONTAL:
-		if direction == SelectorDirection.FORWARD:
-			$Selector.position.y += _warp_amount if $Selector.position.y < 24 else _loop_warp
-		elif  direction == SelectorDirection.BACKWARD:
-			$Selector.position.y -= _warp_amount if $Selector.position.y > -24 else _loop_warp
-	elif _current_orientation == Orientation.VERTICAL:
-		if direction == SelectorDirection.FORWARD:
-			$Selector.position.x += _warp_amount if $Selector.position.x < 24 else _loop_warp
-		elif direction == SelectorDirection.BACKWARD:
-			$Selector.position.x -= _warp_amount if $Selector.position.x > -24 else _loop_warp
-
-func change_selector_orientation():
-	var position_bckp = $Selector.position
 	
-	$Selector.position.x = position_bckp.y
-	$Selector.position.y = position_bckp.x
+	lock_array.shuffle()
 
-	if _current_orientation == Orientation.HORIZONTAL:
-		$Selector.rotation_degrees = 90.0
-		_current_orientation = Orientation.VERTICAL
-	else:
-		$Selector.rotation_degrees = 0
-		_current_orientation = Orientation.HORIZONTAL
-
-func get_selector_orientation() -> Orientation:
-	return _current_orientation
-
-func shuffle_matrix():
-	_lock_array.shuffle()
-
-	for i in _lock_array.size():
-		_lock_matrix[i % 3][i / 3] = _lock_array[i]
+	for i in lock_array.size():
+		_lock_matrix[i % 3][i / 3] = lock_array[i]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var new_lock: Node
-	var new_lock_position = position - Vector2(24.0, 24.0)
+	var new_lock_position = $GateShape.position - Vector2(24.0, -24.0)
 
 	shuffle_matrix()
 
-	for line in _lock_matrix:
-		for lock in line:
-			match lock:
+	for row_i in _lock_matrix.size():
+		for column_i in _lock_matrix[row_i].size():
+			match _lock_matrix[row_i][column_i]:
 				Definitions.LockColors.RED:
-					new_lock = preload("res://actors/common/Act_Common_RedLock.tscn").instantiate()
+					_lock_node_matrix[row_i][column_i] = preload("res://actors/common/Act_Common_RedLock.tscn").instantiate()
 
 				Definitions.LockColors.GREEN:
-					new_lock = preload("res://actors/common/Act_Common_GreenLock.tscn").instantiate()
+					_lock_node_matrix[row_i][column_i] = preload("res://actors/common/Act_Common_GreenLock.tscn").instantiate()
 
 				Definitions.LockColors.BLUE:
-					new_lock = preload("res://actors/common/Act_Common_BlueLock.tscn").instantiate()
+					_lock_node_matrix[row_i][column_i] = preload("res://actors/common/Act_Common_BlueLock.tscn").instantiate()
 
-			new_lock.position = new_lock_position
-			add_child(new_lock)
+			_lock_node_matrix[row_i][column_i].position = new_lock_position
+			add_child(_lock_node_matrix[row_i][column_i])
 
 			new_lock_position.x += 24
-		
+
 		new_lock_position.x -= 72.0
 		new_lock_position.y -= 24.0
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
